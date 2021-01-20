eeca5976907016ce2b17b2f22325d0c20afdef82
吴志豪 <zhihao.wu@ttpai.cn>
2021-01-20 17:01:32 +0800

    readme

--- app/src/main/java/com/andjacoco/demo/MyApp.java
+++ app/src/main/java/com/andjacoco/demo/MyApp.java
@@ -13,7 +13,7 @@ public class MyApp extends Application {
         app = this;
         int a=0;
 
-        CodeCoverageManager.init(app,"demo",BuildConfig.VERSION_CODE);
+        CodeCoverageManager.init(app,BuildConfig.appName,BuildConfig.VERSION_CODE,"http://10.10.17.105:8080");//内网 服务器地址);
         CodeCoverageManager.uploadData();
     }
 
